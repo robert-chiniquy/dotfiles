347354b5ed7274fe111ae5032839ef3a779cbbdb
Robert Chiniquy <robert.chiniquy@rackspace.com>
2014-12-05 13:58:55 -0800

    .inputrc

--- install.sh
+++ install.sh
@@ -9,16 +9,18 @@ UNLINK_DIR_FLAG='-d'
 
 [ `which locale-gen` ] && locale-gen en_US.UTF-8
 
-[ -e .git ] || ( echo ":/" ; exit 1 ) 
+[ -e .git ] || ( echo ":/" ; exit 1 )
 
 [ -h ~/.vim ] && unlink $UNLINK_DIR_FLAG ~/.vim
 [ -h ~/bin ] && unlink $UNLINK_DIR_FLAG ~/bin
 [ -h ~/.vimrc ] && unlink ~/.vimrc
-[ -h ~/bash_login ] && unlink ~/.bash_login
+[ -h ~/.bash_login ] && unlink ~/.bash_login
+[ -h ~/.inputrc ] && unlink ~/.inputrc
 
-[ ! -e ~/.vimrc ] && ln -s `pwd`/.vimrc ~/.vimrc
 [ ! -e ~/.vim ] && ln -s `pwd`/.vim ~/.vim
-[ ! -e ~/.bash_login ] && ln -s `pwd`/.bash_login ~/.bash_login
 [ ! -e ~/bin ] && ln -s `pwd`/bin ~/bin
+[ ! -e ~/.vimrc ] && ln -s `pwd`/.vimrc ~/.vimrc
+[ ! -e ~/.bash_login ] && ln -s `pwd`/.bash_login ~/.bash_login
+[ ! -e ~/.inputrc ] && ln -s `pwd`/.inputrc ~/.inputrc
 
 git submodule update --init
